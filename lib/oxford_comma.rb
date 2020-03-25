f69665260cd3b83da3ab844693c9b8cc362ea980
Bala Luguja <bluguja@gmail.com>
2020-03-25 21:36:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 
  array = ["kiwi", "durian","starfruit"]# define the array global
 def oxford_comma(array)
+<<<<<<< HEAD
   if array.length == 2
   array[-2] << " and "
   array.join
@@ -11,5 +12,8 @@ def oxford_comma(array)
 	array.join(", ")
   end
 end
+=======
+  arr =[]
+>>>>>>> 5023e2d74bdc4e81be1e5159be0399c26c9ff9c7
 
 oxford_comma(array)
